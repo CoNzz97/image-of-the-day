604a82db7715db33f890b9200360bc3710a48145
CoNzz97 <connorbar@hotmail.com>
2023-06-04 11:16:52 +0100

    Add backend to streamlit site

--- Image_Of_The_Day.py
+++ Image_Of_The_Day.py
@@ -1,4 +1,9 @@
 import streamlit as st
+from backend import image_title, image_of_the_day, image_explanation
 
-st.title("Place holder")
+st.set_page_config("wide")
+st.title(image_title)
+st.image(image_of_the_day)
+st.write(image_explanation)
+
 
