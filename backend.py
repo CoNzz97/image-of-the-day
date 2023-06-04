62d426d760614a5a09a5abf5c05e39c994460b04
CoNzz97 <connorbar@hotmail.com>
2023-06-04 11:16:26 +0100

    Implement backend

--- backend.py
+++ backend.py
@@ -3,5 +3,18 @@ import os
 import dotenv
 
 dotenv.load_dotenv("B:/Coding/Python/EnviromentVariables/.env") # Load .env file
-
+APOD_URL = "https://api.nasa.gov/planetary/apod"
 NASA_API = os.getenv("aiod_nasa_open_api_key") # Load API key
+params = {
+    "api_key": NASA_API,
+}
+
+with requests.get(APOD_URL, params) as request:
+    request.raise_for_status()
+    returned = request.json()
+
+image_of_the_day = returned["url"]
+image_title = returned["title"]
+image_explanation = returned["explanation"]
+
+print(image_explanation)
