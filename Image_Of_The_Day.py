2620c107f3afa4e0bbb18647287300f7c8780a31
CoNzz97 <connorbar@hotmail.com>
2023-06-04 11:28:55 +0100

    Added page title Added columns to center content

--- Image_Of_The_Day.py
+++ Image_Of_The_Day.py
@@ -1,9 +1,12 @@
 import streamlit as st
 from backend import image_title, image_of_the_day, image_explanation
 
-st.set_page_config("wide")
-st.title(image_title)
-st.image(image_of_the_day)
-st.write(image_explanation)
+st.set_page_config(layout="wide", page_title="Image of the day.")
+empty_col1, col1, empty_col2 = st.columns(3)
+
+with col1:
+    st.title(image_title)
+    st.image(image_of_the_day)
+    st.write(image_explanation)
 
 
